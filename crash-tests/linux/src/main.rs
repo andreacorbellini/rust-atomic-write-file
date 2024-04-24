e1712fb5f203ef1a80ce76807debd42ec6a5ef3a
Andrea Corbellini <corbellini.andrea@gmail.com>
2024-04-24 14:20:39 -0700

    Use the new `directory()` method in the Linux crash test

--- crash-tests/linux/src/main.rs
+++ crash-tests/linux/src/main.rs
@@ -1,23 +1,22 @@
 use atomic_write_file::AtomicWriteFile;
-use nix::fcntl::open;
-use nix::fcntl::OFlag;
-use nix::sys::stat::Mode;
 use nix::unistd::close;
 use nix::unistd::fsync;
 use std::io::Write;
+use std::os::fd::AsFd;
+use std::os::fd::AsRawFd;
 
 fn main() {
     let mut file = AtomicWriteFile::open("/test/file").expect("open failed");
+    let dir = file
+        .directory()
+        .expect("could not obtain directory fd")
+        .as_fd()
+        .try_clone_to_owned()
+        .expect("could not duplicate directory fd");
+
     file.write_all(b"hello").expect("write failed");
     file.commit().expect("commit failed");
 
-    let dir = open(
-        "/test",
-        OFlag::O_DIRECTORY | OFlag::O_CLOEXEC,
-        Mode::empty(),
-    )
-    .expect("open directory failed");
-
-    fsync(dir).expect("directory sync failed");
-    close(dir).expect("closing directory failed");
+    fsync(dir.as_raw_fd()).expect("directory sync failed");
+    close(dir.as_raw_fd()).expect("closing directory failed");
 }
